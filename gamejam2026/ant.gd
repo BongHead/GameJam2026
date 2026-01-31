7ad14a76c658c553a022a35c070922735f5ce3ae
Cyrus Yip <cyrusyip@cyrusyip.com>
2026-01-31 14:35:42 -0500

    ant idle

--- gamejam2026/ant.gd
+++ gamejam2026/ant.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+var idle = true
+var next_flip = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +13,16 @@ func _process(delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	position.x -= 200 * delta
+	next_flip -= delta
+	if next_flip <= 0:
+		linear_velocity.x = - linear_velocity.x
+
+		if global_position.distance_to(Vector2.ZERO) > 250:
+			linear_velocity = global_position.direction_to(Vector2.ZERO) * 150
+		if randf() < 0.2:
+			linear_velocity.x = randf_range(-200, 200)
+			linear_velocity.y = randf_range(-60, 60)
+
+		$AnimatedSprite2D.flip_h = linear_velocity.x > 0
+
+		next_flip = randf_range(0.2, 1.3)
