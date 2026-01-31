63cb9817fece7cbceabcd96a85fc447449a26c60
wensong1122 <yuwensong04@gmail.com>
2026-01-31 12:31:51 -0500

    update: remove input event from world.gd

--- gamejam2026/world.gd
+++ gamejam2026/world.gd
@@ -47,9 +47,3 @@ func get_random_vector(min, max)->Vector2:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			print("test")	
