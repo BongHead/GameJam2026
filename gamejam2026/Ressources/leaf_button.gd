be8a5eb4a3b280fa9ecedcbdcf13ce1a26598a95
Victor <xvictearx@gmail.com>
2026-02-01 09:41:07 -0500

    forgor file

--- gamejam2026/Ressources/leaf_button.gd
+++ gamejam2026/Ressources/leaf_button.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
 @onready var _pcon = $Background
+@onready var material_count = MATERIAL_CONSTANT.MaterialCount.LOW
+
 
 
 func _on_button_pressed() -> void:
 	var _antcount = get_parent().ants
 	$Button/AudioStreamPlayer2D.play()
 	print("leaf pressed")
+	print(material_count)
 	_pcon.visible = not _pcon.visible
 	
 	$Background/VBoxContainer/HSlider_worker.max_value = _antcount
@@ -16,3 +19,12 @@ func _on_gather_pressed() -> void:
 	var num = $Background/VBoxContainer/HSlider_worker.value
 	var location = position
 	get_parent().send_ants(num, location)
+
+func take_material(take_amount):
+	if material_count - take_amount <= 0:
+		_pcon.visible = not _pcon.visible
+		return material_count - take_amount
+	else:
+		material_count -= take_amount
+	
+	return take_amount
