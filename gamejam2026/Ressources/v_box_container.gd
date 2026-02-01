6d6da5ea4961a6772c85fee727e5e00ee02a3965
Sarkazzm <lix44556@gmail.com>
2026-01-31 19:17:15 -0500

    popup menu changes

--- gamejam2026/Ressources/v_box_container.gd
+++ gamejam2026/Ressources/v_box_container.gd
@@ -1,11 +1,14 @@
 extends VBoxContainer
 
-@onready var slider_label = $slider_label
-@onready var h_slider = $HSlider
+@onready var slider_label_worker = $slider_label_worker
+@onready var h_slider_worker = $HSlider_worker
+@onready var slider_label_warrior = $slider_label_warrior
+@onready var h_slider_warrior = $HSlider_warrior
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	slider_label.text = str(h_slider.value)
+	slider_label_worker.text = str(h_slider_worker.value)
+	slider_label_warrior.text = str(h_slider_warrior.value)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,4 +17,9 @@ func _process(delta: float) -> void:
 
 
 func _on_h_slider_value_changed(value: float) -> void:
-	slider_label.text = str(value)
+	slider_label_worker.text = str(h_slider_worker.value)
+	slider_label_warrior.text = str(h_slider_warrior.value)
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
